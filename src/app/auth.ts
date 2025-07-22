6b2562ecb59ff231d027af23c60e73080e5955f4
msr <msrxse@gmail.com>
2025-07-22 20:21:44 +0100

    Fixes to auth not accessing variables

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,10 +1,9 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-
-import { accounts, sessions, users, verificationTokens } from "@/db/schema";
-import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import { accounts, sessions, users, verificationTokens } from "@/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db, {
@@ -17,5 +16,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     error: "/login",
     signIn: "/login",
   },
-  providers: [GitHub, Google],
+  providers: [Github, Google],
 });
