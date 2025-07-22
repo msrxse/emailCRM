838e2949c7f2816f95cc6ba2a2808fb0d4eb0012
msr <msrxse@gmail.com>
2025-07-22 12:27:21 +0100

    Add next-auth and its provider

--- src/app/(platform)/home/page.tsx
+++ src/app/(platform)/home/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function HomePage() {
+  const { data: session, status } = useSession();
+  console.log(session, status);
+
   return <div>Home</div>;
 }
